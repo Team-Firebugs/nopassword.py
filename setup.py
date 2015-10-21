457a617f06f3a654976d756805597adac3c8c02f
oskarnyqvist <oskarnyqvist@gmail.com>
2015-10-21 21:37:26 +0200

    updated for argument

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("requirements.txt") as f:
 
 setup(
     name='nopassword',
-    version='0.1.0',
+    version='0.1.1',
     description='Determenistic password generator',
     long_description=read_md('README.md'),
     #long_description=readme,
